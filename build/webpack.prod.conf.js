1923fa81eb697b2fb3fb3f9c484bf67d797055dd
Philipp Mitterer <philipp.mitterer@e-mundo.de>
2018-06-27 07:04:55 +0200

    removes HTML server build

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,48 +1,110 @@
-'use strict'
-// const path = require('path')
-const utils = require('./utils')
-const webpack = require('webpack')
-const config = require('../config')
-const merge = require('webpack-merge')
-const baseWebpackConfig = require('./webpack.base.conf')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+'use strict';
+const path = require('path');
+const utils = require('./utils');
+const webpack = require('webpack');
+const config = require('../config');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const vueLoaderConfig = require('./vue-loader.conf');
 
-const env = require('../config/prod.env')
+const env = require('../config/prod.env');
 
-const webpackConfig = merge(baseWebpackConfig, {
-  entry: './src/custom-script.js',
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+const webpackConfig = {
+  context: path.resolve(__dirname, '../'),
+  entry: './src/index.js',
   module: {
-    rules: utils.styleLoaders({
-      sourceMap: config.build.productionSourceMap,
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ].concat(utils.styleLoaders({
       extract: true,
       usePostCSS: true
-    })
+    }))
   },
   output: {
     library: 'customScript',
     libraryTarget: 'amd',
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('[name].[chunkhash].js')
+    filename: process.env.NODE_ENV === 'production' ? '[name].[chunkhash].js' : 'main.js'
+  },
+  devServer: {
+    contentBase: config.build.assetsRoot,
+    compress: true,
+    port: 8080,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src'),
+    }
+  },
+  node: {
+    // prevent webpack from injecting useless setImmediate polyfill because Vue
+    // source contains it (although only uses it if it's native).
+    setImmediate: false,
+    // prevent webpack from injecting mocks to Node native modules
+    // that does not make sense for the client
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
     }),
-    // new UglifyJsPlugin({
-    //   uglifyOptions: {
-    //     compress: {
-    //       warnings: false
-    //     }
-    //   },
-    //   sourceMap: config.build.productionSourceMap,
-    //   parallel: true
-    // })
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false
+        }
+      },
+      sourceMap: config.build.productionSourceMap,
+      parallel: true
+    })
   ]
-});
+};
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
