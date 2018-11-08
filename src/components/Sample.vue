88a895c01377610b4a95037f6be2494fd1f4c593
davidhellmann <david@hellmann.io>
2018-11-08 15:21:55 +0100

    Add Sample Vue
    Signed-off-by: davidhellmann <david@hellmann.io>

---
+++ src/components/Sample.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="`${cn} ${setModifiers}`">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Sample',
+    props: {
+      modifiers: {
+        type: Array,
+        default: () => []
+      },
+    },
+    data () {
+      return {
+        cn: 'fr-sample',
+        msg: 'I\'m a Sample Component'
+      }
+    },
+    computed: {
+      setModifiers() {
+        return this.modifiers.map(mod => this.cn + "--" + mod).join("  ");
+      }
+    },
+    methods: {},
+    watch: {},
+    mounted() {},
+    created() {},
+    components: {},
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
