18d8da49f75d52ffa2d621254d381a3289040169
davidhellmann <david@hellmann.io>
2019-02-05 09:56:43 +0100

    Remove Server Name etc
    Signed-off-by: davidhellmann <david@hellmann.io>

--- rsync.sh
+++ rsync.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-SOURCE="./dist/*"
-USER="web05025"
-SERVER="web05.microsites.redbull.com"
-FOLDER="/stage/test"
 
-# rsync -avz --password-file=./rsync_pass "$SOURCE" "$USER@$SERVER":"$FOLDER"
-rsync -avz --password-file=./rsync_pass ./dist/* web05025@web05.microsites.redbull.com:/stage/test
+rsync -avz ./dist/* USER@SERVER:/FOLDER
